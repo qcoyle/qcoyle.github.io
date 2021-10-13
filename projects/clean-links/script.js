cc92e4ee3d243625c36df250161f25a09d05902a
qcoyle <quinn.t.coyle@gmail.com>
2021-10-13 11:01:44 -0500

    separate modules

--- projects/clean-links/script.js
+++ projects/clean-links/script.js
@@ -1,9 +1,10 @@
-const inputElement = document.getElementById("input");
-inputElement.addEventListener("change", handleFiles, false);
+import { run } from './modify-text.js';
+
+const inputElement = document.getElementById("input"); // Object for HTML input element
+inputElement.addEventListener("change", handleFiles, false); // Do this once input is clicked
 
 function handleFiles() {
-    const fileList = this.files; /* now you can work with the file list */
-    const numFiles = fileList.length;
+    const fileList = this.files;
 
     for (let i = 0, numFiles = fileList.length; i < numFiles; i++) {
         const file = fileList[i];
@@ -11,37 +12,12 @@ function handleFiles() {
     }
 }
 
-function run(file, flag) {
-    var startIndex = 0,
-        index;
-
-    while ((index = file.indexOf(flag, startIndex)) > -1) {
-        file = removeHead(index, file, flag);
-        file = removeEnd(index, file);
-    }
-
-    return file;
-}
-
-function removeHead(index, file, flag) {
-    return (file.substr(0, index) + file.substr(index + flag.length));
-}
-
-function removeEnd(index, file) {
-    startIndex = file.indexOf("&amp", index);
-    endIndex = file.indexOf('"', startIndex);
-    return file.substring(0, startIndex) + file.substring(endIndex);
-}
-
 function download(filename, text) {
     var element = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
     element.setAttribute('download', filename);
-
     element.style.display = 'none';
     document.body.appendChild(element);
-
     element.click();
-
     document.body.removeChild(element);
 }
