612cdda4b1253cf0a3d1f9223e677499022bcbf3
qcoyle <quinn.t.coyle@gmail.com>
2021-10-28 08:56:27 -0500

    add age range calculator

---
+++ projects/age-calculator/app.js
@@ -0,0 +1,35 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        age: "",
+        submitted: false
+    },
+    computed: {
+        ageLevel: {
+            get: function() {
+                if (this.age >= 0) {
+                    if (this.age < 18) {
+                        return "Child";
+                    } else if (this.age < 35) {
+                        return "Young adult";
+                    } else if (this.age < 65) {
+                        return "Adult";
+                    } else {
+                        return "Senior";
+                    }
+                }
+            },
+            set: function(newAgeLevel) {
+                if (newAgeLevel === "Child") {
+                    this.age = 0;
+                } else if (newAgeLevel === "Young adult") {
+                    this.age = 18;
+                } else if (newAgeLevel === "Adult") {
+                    this.age = 35;
+                } else if (newAgeLevel === "Senior") {
+                    this.age = 65;
+                }
+            }
+        }
+    }
+});
